1355479ea26e35a84c6ae2e5a9d44d709761b02a
kurniawan2134 <87314129+kurniawan2134@users.noreply.github.com>
2021-07-13 01:09:48 +0700

    Create redir.js

---
+++ redir.js
@@ -0,0 +1,11 @@
+var urlmain = document.URL;
+var md = document.URL.split("/")[3];
+var key = document.URL.split("/")[4];
+var id = document.URL.split("/")[5];
+
+if (md== 'deep') {
+	var redir = 'https://s.click.aliexpress.com/deep_link.htm?
+aff_short key='+key+'%dl_target_url=https://www.aliexpress.com/item/'+id+'';
+}  else {
+   var redir = 'https://s.click.aliexpress.com/e/'+key;
+}
